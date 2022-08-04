852682d2070f7980019790ec715699d3f2fe7982
KostiaevSV <skprod.info@gmail.com>
2022-08-04 16:16:02 +0400

    HW done ShopItemClass

---
+++ src/models/item.js
@@ -0,0 +1,14 @@
+export class Item { 
+    constructor(entity) { 
+        Object.assign (this, {
+            brand : '',
+            title : '', 
+            description : '', 
+            descriptionFull : '', 
+            price : '', 
+            currency : '', 
+        }, entity); 
+    }
+}
+
+export default Item;
